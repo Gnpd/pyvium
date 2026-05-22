085024ccd3ca71b28945e14a60ace9203c721889
Alejandro Gutiérrez <npdsolutions@gmail.com>
2026-05-22 20:23:33 +0200

    feat(pyvium/__init__.py): Add BatchModeFunctions to Pyvium

--- pyvium/pyvium/__init__.py
+++ pyvium/pyvium/__init__.py
@@ -1,7 +1,8 @@
+from .batch_mode_functions import BatchModeFunctions
 from .direct_mode_functions import DirectModeFunctions
 from .generic_functions import GenericFunctions
 from .method_mode_functions import MethodModeFunctions
 
 
-class Pyvium(DirectModeFunctions, GenericFunctions, MethodModeFunctions):
+class Pyvium(BatchModeFunctions, DirectModeFunctions, GenericFunctions, MethodModeFunctions):
     pass
