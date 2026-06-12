51842007d972efa2f0ce3637d993828008658ba0
Alejandro Gutiérrez <npdsolutions@gmail.com>
2026-06-12 12:10:38 +0200

    feat(pyvium): add orphan discovery and cleanup to the instance manager

--- pyvium/__init__.py
+++ pyvium/__init__.py
@@ -1,4 +1,5 @@
 from .core import Core
 from .pyvium import Pyvium
 from .tools import Tools
-from .instance_manager import IviumsoftInstanceManager, ManagedInstance
+from .instance_manager import (DiscoveryReport, IviumsoftInstanceManager,
+                               ManagedInstance, UntrackedProcess)
